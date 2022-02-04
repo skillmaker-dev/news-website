3edb54115e85f779b50bedf6560c774fc32154b2
Anas CHAHID <anas.chahid777@gmail.com>
2022-02-04 02:17:15 +0100

    fixed security

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -6,14 +6,14 @@ export default function Footer() {
   return <footer className="myFooter">
         <div className="footer-container">
             <ul>
-                <li><a href="https://developer.nytimes.com" target="_blank"><img src={nytimes} alt="nytimes logo" /></a></li>
+                <li><a href="https://developer.nytimes.com" rel="noreferrer" target="_blank"><img src={nytimes} alt="nytimes logo" /></a></li>
             </ul>
         <p className="footer-copyright">© 2022 By Anas Chahid</p>
         </div>
         <div className="footer-social">
-            <a href="https://www.github.com/skillmaker-dev" target="_blank" className="social-icons"><i className="fa fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/anas-chahid/" target="_blank" className="social-icons"><i className="fa fa-linkedin"></i></a>
-            <a href="https://www.instagram.com/iamanaschahid/" target="_blank" className="social-icons"><i className="fa fa-instagram"></i></a>
+            <a href="https://www.github.com/skillmaker-dev" target="_blank" rel="noreferrer" className="social-icons"><i className="fa fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/anas-chahid/" target="_blank" rel="noreferrer" className="social-icons"><i className="fa fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/iamanaschahid/" target="_blank" rel="noreferrer" className="social-icons"><i className="fa fa-instagram"></i></a>
         </div>
     </footer>
   ;
--- src/pages/about-page/about-page.jsx
+++ src/pages/about-page/about-page.jsx
@@ -4,7 +4,7 @@ const AboutPage = () =>
 (
     <div className="about-container">
         <p>This website was created by Anas Chahid Using React.js and New York Time API</p>
-        <p>Visit my personal portfolio for more projects <a href="https://anaschahid.netlify.app/" target="_blank">Website</a></p>       
+        <p>Visit my personal portfolio for more projects <a href="https://anaschahid.netlify.app/" rel="noreferrer" target="_blank">Website</a></p>       
     </div>
 )
 
